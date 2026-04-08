2c7482ca56e8ca2774c454d445e4716f4633f942
Rajiv Ranjan <rajranja@redhat.com>
2026-04-08 17:48:06 +0530

    initial code commit

--- ingestion.py
+++ ingestion.py
@@ -2,15 +2,16 @@ import os
 
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_text_splitters import CharacterTextSplitter
+
 
 load_dotenv()
 
 if __name__ == "__main__":
     print("Ingesting...")
-    loader = TextLoader("/Users/edenmarco/Desktop/langchain-course/mediumblog1.txt")
+    loader = TextLoader("./mediumblog1.txt")
     document = loader.load()
 
     print("splitting...")
